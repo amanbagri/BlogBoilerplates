109a141e3f9003bfa7db607f3b98f31728885094
Aman Bagri <amanbagri95@gmail.com>
2023-09-25 13:23:36 +0530

    update

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/(.*)#$',
+                destination: '/about',
+                permanent: true,
+            },
+        ]
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+
+
+}
 
 module.exports = nextConfig
